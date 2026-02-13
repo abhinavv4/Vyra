1024d518b60ea106258121ff306185de4b74a0a2
Abhinav <abhinav4077@gmail.com>
2026-02-13 15:54:29 +0530

    Delete VYRA directory

--- VYRA/lib/format.ts
+++
@@ -1,6 +0,0 @@
-﻿export const formatCurrency = (value: number) =>
-  new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    maximumFractionDigits: 0,
-  }).format(value);
